9ac356c39d6d560a92bba7fd3fdce4834a0635ae
qianfeng <qianfeng@aaaaaaaaaaaaaaa.local>
2015-07-15 13:41:07 +0800

    Initial Commit

---
+++ 手工客1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  手工客1
+//
+//  Created by qianfeng on 15-7-15.
+//  Copyright (c) 2015年 gzc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 手工客1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  手工客1
+//
+//  Created by qianfeng on 15-7-15.
+//  Copyright (c) 2015年 gzc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
